9f48c341eac2f115ff02c1a001a0250c2360e954
fleitedeso <felipe_leitesouza@hotmail.com>
2023-04-13 13:18:55 -0300

    criando um servidor de teste

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"COntent-Type": "text/plan"});
+    res.end("Testando Node");
+});
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
+})
