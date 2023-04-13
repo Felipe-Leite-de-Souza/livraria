bb1b3bcda129fd5bef4f4ba36ac6d5fc1981d2f3
fleitedeso <felipe_leitesouza@hotmail.com>
2023-04-13 13:27:36 -0300

    adicionado rotas

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+    "/": "Teste server node",
+    "/livros": "Página de livros",
+    "/autores": "Lista de autores",
+    "/editora": "Página de editora",
+    "/sobre": "informações sobre a livraria",
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"COntent-Type": "text/plan"});
-    res.end("Testando Node");
+    res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
